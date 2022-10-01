c29dd5423ea748f015b591622f88834cb2dfac66
Yunus Emre Özköse <yunusemre.ozkose.@sestek.com>
2022-10-01 23:53:01 +0300

    fix minors

--- b5/client.cpp
+++ b5/client.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char** argv){
 	auto client = CLIENT();
 
-	std::string image_path = "/home/sestek/workspace/medium/cpp_series/data/bee_ant/hymenoptera_data/bees/16838648_415acd9e3f.jpg";
+	std::string image_path = "/path/to/hymenoptera_data/bees/16838648_415acd9e3f.jpg";
     cv::Mat image = cv::imread(image_path);
 	cv::resize(image, image, cv::Size( IM_WIDTH , IM_HEIGHT ));
 
